10fc0f1d4019fa341b7e5a355221b1f55986ef1b
hyosung <67565847+bearscome@users.noreply.github.com>
2022-09-05 15:44:10 +0900

    create todoList
    origin: https://jeffgukang.github.io/react-native-tutorial/docs/basic-tutorial/basic-features(todolist)/03-cardview/cardview-kr.html

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
@@ -31,7 +32,10 @@ const App = () => {
   }, []);
   return (
     <SafeAreaView style={styles.container}>
-      <Text>hello word!</Text>
+      <Text style={styles.appTitle}>hello TodoList!</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder="add an item" />
+      </View>
     </SafeAreaView>
   );
 };
@@ -39,9 +43,31 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#3143e8',
+  },
+  appTitle: {
+    color: '#fff',
+    fontSize: 36,
+    marginTop: 30,
+    marginBottom: 30,
+    fontWeight: '300',
+    textAlign: 'center',
+    backgroundColor: '#3143e8',
+  },
+  card: {
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+    borderTopLeftRadius: 10, // to provide rounded corners
+    borderTopRightRadius: 10, // to provide rounded corners
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 24,
+    marginLeft: 20,
   },
 });
 
