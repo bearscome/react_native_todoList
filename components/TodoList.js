ec716bd06f0c3ad42c4810e29b2894b864ccad41
hyosung <67565847+bearscome@users.noreply.github.com>
2022-09-05 16:31:40 +0900

    TouchableOpacity
    TouchableOpacity: Button 컴포넌트는 ios, android에 따라 버튼이 다르게 보여 관리하기 힘들다. 때문에 TouchableOpacity를 사용한다.

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,11 +1,12 @@
 // 추가된 아이템을 스크롤 뷰를 통해 보여주는 부분이다.
 import React from 'react';
 import {StyleSheet, ScrollView, Text} from 'react-native';
+import TodoListItem from './TodoListItem';
 
 const TodoList = () => {
   return (
     <ScrollView contentContainerStyle={styles.listContainer}>
-      <Text>TodoList</Text>
+      <TodoListItem />
     </ScrollView>
   );
 };
